4a564c8541ccfd66b4eda16341dfb304acbce070
Jovan Gerodetti <jovan.gerodetti@titannano.de>
2022-03-10 23:44:36 +0100

    clean up helicopter behavior

--- AudioSource.gd
+++ AudioSource.gd
@@ -14,27 +14,29 @@ var track_name setget ,_get_track_name
 
 func _process(delta):
 	if not active_slot or not target_slot:
-		return 
+		return
 
-	if not active_slot.playing or target_slot.playing:
+	if not active_slot.playing or not target_slot.playing:
 		target_slot.volume_db = 0
 		return
-		
+
 	self.elapsed += delta
 
-	var current_dB = clamp(self.elapsed / DURATION * -80, -80, 0)
+	var current_dB = clamp((self.elapsed / DURATION) * -80, -80, 0)
 
-	target_slot.volume_dB = current_dB
-	active_slot.volume_dB = -80 - current_dB
+	active_slot.volume_db = current_dB
+	target_slot.volume_db = -80 - current_dB
 
-	if active_slot.volume_dB <= -80:
+	prints("current_dB: ", target_slot.volume_db, active_slot.volume_db)
+
+	if active_slot.volume_db <= -80:
 		active_slot.stop()
 
 
 func play_track(track: AudioStream):
 	self.active_slot = slot0 if slot0.playing else slot1
 	self.target_slot = slot1 if slot1 != active_slot else slot0
-	
+
 	self.target_slot.stream = track
 	self.target_slot.play(0)
 
@@ -48,9 +50,9 @@ func play_track(track: AudioStream):
 
 func _get_playing() -> bool:
 	return self.slot0.playing or self.slot1.playing
-	
+
 
 func _get_track_name() -> String:
 	var stream := self.target_slot.stream if self.target_slot else null
-	
+
 	return stream.resource_path if stream else ""
---
+++ Objects/Helis/Rotor.gd
@@ -0,0 +1,25 @@
+extends Spatial
+
+export(float, 0, 1) var power := 1.0
+export var rpm := 471
+
+var previous_rotation := Vector3.ZERO
+
+onready var mesh := self.get_child(0)
+onready var material: ShaderMaterial = mesh.get_surface_material(0)
+
+func _physics_process(delta: float) -> void:
+	var speed = (self.rpm / 60 * delta) * 360 * self.power
+
+	if speed == 0:
+		return
+
+	self.rotate_y(deg2rad(speed))
+
+func _process(_delta: float) -> void:
+	#rotor velocity
+	var rotation_offset: float = abs(self.rotation.y - self.previous_rotation.y)
+	var previous_transform := Transform.IDENTITY.rotated(Vector3.FORWARD, min(rotation_offset, 0.610865) * -1)
+
+	self.previous_rotation = Vector3(self.rotation)
+	self.material.set_shader_param("previous_transform", previous_transform)

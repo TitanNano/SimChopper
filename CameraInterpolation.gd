ae3d5819b5c2010be317593a14d4e7e3818b13eb
Jovan Gerodetti <jovan.gerodetti@titannano.de>
2022-03-23 23:45:40 +0100

    cleanup CameraInterpolation

--- CameraInterpolation.gd
+++ CameraInterpolation.gd
@@ -7,12 +7,12 @@ export(float, 0, 1) var speed: float = 0.5
 
 func _ready():
 	var target = get_node(self.remote_path) as Spatial
-	
+
 	self.local_transform = self.transform
-	
+
 	if not target:
 		return
-		
+
 	target.global_transform = self.global_transform
 
 
@@ -25,14 +25,14 @@ func _physics_process(_delta: float):
 
 	if not self.full_transform:
 		var parent := self.get_parent() as Spatial
-		
+
 		var counter_transform = Transform() \
 			.rotated(Vector3.RIGHT, -parent.rotation.x) \
 			.rotated(Vector3.BACK, -parent.rotation.z)
-			
+
 		self.global_transform = parent.global_transform * counter_transform * self.local_transform
-		
-	
+
+
 #	var current_rotation := target.rotation
 	var current_transform := target.global_transform
 	var destination_transform := self.global_transform
@@ -43,6 +43,6 @@ func _physics_process(_delta: float):
 #	self.speed = min(1.0, max(self.speed, 0.1))
 
 	target.transform = current_transform.interpolate_with(destination_transform, self.speed)
-	
+
 	# if not full_transform:
 	#	target.rotation = Vector3(current_rotation.x, target.rotation.y, current_rotation.z)

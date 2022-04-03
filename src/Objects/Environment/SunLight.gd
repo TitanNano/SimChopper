5c3c305295beaf877b2dbeed785f740cb65c1b55
Jovan Gerodetti <jovan.gerodetti@titannano.de>
2022-04-03 23:32:48 +0200

    sky should update sun
    The sun color should be adopted from the sky.

--- src/Objects/Environment/SunLight.gd
+++ src/Objects/Environment/SunLight.gd
@@ -6,13 +6,14 @@ func _process(_delta):
 
 	if not parent_env is WorldEnvironment:
 		return
-	
+
 	var sky: ProceduralSky = parent_env.environment.background_sky;
-	
+
 	if not sky is ProceduralSky:
 		return
-	
+
 	var sun_x = sky.sun_latitude * -1.0
-	var sun_y = 180.0 - sky.sun_longitude 
-	
+	var sun_y = 180.0 - sky.sun_longitude
+
 	self.rotation = Vector3(deg2rad(sun_x), deg2rad(sun_y), 0)
+	self.light_color = sky.sun_color

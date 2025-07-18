5e15a3176684c75924c086d428e65dc317aebdab
Jovan Gerodetti <jovan.gerodetti@titannano.de>
2025-07-19 01:03:13 +0200

    Physical lighting and practical camera are incompatible (#87)
    * Physical lighting and practical camera are incompatible
    
    * fix day length & SDFGI range
    
    * Fix day-night transitions

--- native/src/scripts/objects/camera.rs
+++ native/src/scripts/objects/camera.rs
@@ -1,9 +1,12 @@
-use godot::classes::Camera3D;
+use godot::builtin::math::FloatExt;
 use godot::obj::Gd;
+use godot::{builtin::Signal, classes::Camera3D};
 use godot_rust_script::{godot_script_impl, GodotScript, RsRef};
 
+use crate::script_callable;
 use crate::{
     scripts::world::solar_setup::{ISolarSetup, SolarSetup},
+    util::logger,
     warn,
 };
 
@@ -18,11 +21,19 @@ struct Camera {
 
 #[godot_script_impl]
 impl Camera {
-    const MID_DAY_EXPOSURE: f32 = 90.0;
-    const DAWN_EXPOSURE: f32 = 200.0;
+    const MID_DAY_EXPOSURE: f32 = 10.0;
+    const DAWN_EXPOSURE: f32 = 30.0;
     const NIGHT_EXPOSURE: f32 = 1000.0;
 
-    pub fn _ready(&mut self) {}
+    pub fn _ready(&mut self) {
+        let Some(ref solar_setup) = self.solar_setup else {
+            logger::error!("missing solar setup in Camera!");
+            return;
+        };
+
+        Signal::from_object_signal(solar_setup, "sky_brightness")
+            .connect(&script_callable!(self, Self::_ready), 0);
+    }
 
     pub fn _process(&mut self, _delta: f64) {
         let Some(solar_setup) = self.solar_setup.as_mut() else {
@@ -35,36 +46,68 @@ impl Camera {
             return;
         };
 
-        let game_time = solar_setup.get_ingame_time_h();
+        let sky_brightness = solar_setup.sky_brightness();
+        let sun_zenite_distance = solar_setup.sun_zenite_distance();
 
-        let exposure = match game_time {
-            0.0..6.0 => {
-                Self::DAWN_EXPOSURE
-                    - ((Self::DAWN_EXPOSURE - Self::MID_DAY_EXPOSURE) / 6.0) * game_time as f32
-            }
-            6.0..12.0 => {
-                Self::MID_DAY_EXPOSURE
-                    + ((Self::DAWN_EXPOSURE - Self::MID_DAY_EXPOSURE) / 6.0)
-                        * (game_time as f32 - 6.0)
-            }
-            12.0..13.0 => {
-                Self::DAWN_EXPOSURE
-                    + ((Self::NIGHT_EXPOSURE - Self::DAWN_EXPOSURE) / 6.0)
-                        * (game_time as f32 - 12.0)
-            }
-            13.0..23.0 => Self::NIGHT_EXPOSURE,
-            23.0..24.0 => {
-                Self::NIGHT_EXPOSURE
-                    - ((Self::NIGHT_EXPOSURE - Self::DAWN_EXPOSURE) / 6.0)
-                        * (game_time as f32 - 18.0)
-            }
-            _ => unreachable!("game time is capped to 24h"),
-        };
+        let day_exposure = Self::MID_DAY_EXPOSURE.lerp(Self::DAWN_EXPOSURE, sun_zenite_distance);
+
+        let night_exposure = interpolate(
+            Self::DAWN_EXPOSURE,
+            Self::NIGHT_EXPOSURE,
+            1.0 - sky_brightness,
+        );
+
+        let exposure = if sun_zenite_distance < 1.0 {
+            day_exposure
+        } else {
+            night_exposure
+        }
+        .floor();
 
         if attributes.get_exposure_sensitivity() == exposure {
             return;
         }
 
         attributes.set_exposure_sensitivity(exposure);
+
+        logger::debug!(
+            "Updating camera exposure: {}",
+            attributes.get_exposure_sensitivity()
+        );
+        logger::debug!("sky brightness: {}", sky_brightness);
+    }
+}
+
+fn interpolate(from: f32, to: f32, weight: f32) -> f32 {
+    let range = to - from;
+
+    from + (weight.powf(4.0) * range)
+}
+
+#[cfg(test)]
+mod test {
+    use godot::builtin::math::ApproxEq;
+
+    use super::interpolate;
+
+    #[test]
+    fn qerp_start() {
+        let value = interpolate(4.5, 7.3, 0.0);
+
+        assert_eq!(value, 4.5);
+    }
+
+    #[test]
+    fn qerp_end() {
+        let value = interpolate(4.5, 7.3, 1.0);
+
+        assert_eq!(value, 7.3);
+    }
+
+    #[test]
+    fn qerp_sample() {
+        let value = interpolate(0.0, 1.0, 0.7);
+
+        assert!(value.approx_eq(&0.49));
     }
 }

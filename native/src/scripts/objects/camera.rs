aaf586301f27735eb93c45ef9f543b64c9115c05
Jovan Gerodetti <jovan.gerodetti@titannano.de>
2025-08-23 22:07:05 +0200

    Use camera f-stop and shutter instead of exposure

--- native/src/scripts/objects/camera.rs
+++ native/src/scripts/objects/camera.rs
@@ -1,14 +1,13 @@
 use godot::builtin::math::FloatExt;
+use godot::builtin::Signal;
+use godot::classes::{Camera3D, CameraAttributesPhysical};
 use godot::obj::Gd;
-use godot::{builtin::Signal, classes::Camera3D};
 use godot_rust_script::{godot_script_impl, GodotScript, OnEditor, RsRef};
+use itertools::Itertools;
 
 use crate::script_callable;
-use crate::{
-    scripts::world::solar_setup::{ISolarSetup, SolarSetup},
-    util::logger,
-    warn,
-};
+use crate::scripts::world::solar_setup::{ISolarSetup, SolarSetup};
+use crate::util::logger;
 
 #[derive(GodotScript, Debug)]
 #[script(base = Camera3D)]
@@ -19,11 +18,98 @@ struct Camera {
     base: Gd<Camera3D>,
 }
 
+#[derive(Debug)]
+struct ExposureSetting {
+    fstop: f32,
+    shutter: f32,
+    lux: f32,
+}
+
 #[godot_script_impl]
 impl Camera {
-    const MID_DAY_EXPOSURE: f32 = 10.0;
-    const DAWN_EXPOSURE: f32 = 30.0;
-    const NIGHT_EXPOSURE: f32 = 400.0;
+    const NIGHT_SHUTTER: f32 = 2.0;
+    const NIGHT_FSTOP: f32 = 1.4;
+    const NIGHT_LUX: f32 = 2.2;
+
+    const NIGHT2_SHUTTER: f32 = 2.0;
+    const NIGHT2_FSTOP: f32 = 2.0;
+    const NIGHT2_LUX: f32 = 20.0;
+
+    const NIGHT3_SHUTTER: f32 = 4.0;
+    const NIGHT3_FSTOP: f32 = 2.0;
+    const NIGHT3_LUX: f32 = 40.0;
+
+    const NIGHT4_SHUTTER: f32 = 8.0;
+    const NIGHT4_FSTOP: f32 = 2.0;
+    const NIGHT4_LUX: f32 = 160.0;
+
+    const NIGHT5_SHUTTER: f32 = 15.0;
+    const NIGHT5_FSTOP: f32 = 2.0;
+    const NIGHT5_LUX: f32 = 160.0;
+
+    const NIGHT6_SHUTTER: f32 = 30.0;
+    const NIGHT6_FSTOP: f32 = 2.0;
+    const NIGHT6_LUX: f32 = 320.0;
+
+    const DAWN_SHUTTER: f32 = 60.0;
+    const DAWN_FSTOP: f32 = 5.6;
+    const DAWN_LUX: f32 = 2560.0;
+
+    const MID_DAY_SHUTTER: f32 = 60.0;
+    const MID_DAY_FSTOP: f32 = 22.0;
+    const MID_DAY_LUX: f32 = 81920.0;
+
+    const MAX_LIGHT_SHUTTER: f32 = 60.0;
+    const MAX_LIGHT_FSTOP: f32 = 32.0;
+    const MAX_LIGHT_LUX: f32 = 163840.0;
+
+    const FSTOPS: [ExposureSetting; 9] = [
+        ExposureSetting {
+            fstop: Self::NIGHT_FSTOP,
+            shutter: Self::NIGHT_SHUTTER,
+            lux: Self::NIGHT_LUX,
+        },
+        ExposureSetting {
+            fstop: Self::NIGHT2_FSTOP,
+            shutter: Self::NIGHT2_SHUTTER,
+            lux: Self::NIGHT2_LUX,
+        },
+        ExposureSetting {
+            fstop: Self::NIGHT3_FSTOP,
+            shutter: Self::NIGHT3_SHUTTER,
+            lux: Self::NIGHT3_LUX,
+        },
+        ExposureSetting {
+            fstop: Self::NIGHT4_FSTOP,
+            shutter: Self::NIGHT4_SHUTTER,
+            lux: Self::NIGHT4_LUX,
+        },
+        ExposureSetting {
+            fstop: Self::NIGHT5_FSTOP,
+            shutter: Self::NIGHT5_SHUTTER,
+            lux: Self::NIGHT5_LUX,
+        },
+        ExposureSetting {
+            fstop: Self::NIGHT6_FSTOP,
+            shutter: Self::NIGHT6_SHUTTER,
+            lux: Self::NIGHT6_LUX,
+        },
+        ExposureSetting {
+            fstop: Self::DAWN_FSTOP,
+            shutter: Self::DAWN_SHUTTER,
+            lux: Self::DAWN_LUX,
+        },
+        ExposureSetting {
+            fstop: Self::MID_DAY_FSTOP,
+            shutter: Self::MID_DAY_SHUTTER,
+            lux: Self::MID_DAY_LUX,
+        },
+        ExposureSetting {
+            fstop: Self::MAX_LIGHT_FSTOP,
+            shutter: Self::MAX_LIGHT_SHUTTER,
+            lux: Self::MAX_LIGHT_LUX,
+        },
+    ];
 
     pub fn _ready(&mut self) {
         Signal::from_object_signal(&**self.solar_setup, "sky_brightness")
@@ -31,73 +117,35 @@ impl Camera {
     }
 
     pub fn _process(&mut self, _delta: f64) {
-        let Some(mut attributes) = self.base.get_attributes() else {
-            warn!("Camera has no attributes!");
+        let Some(mut attributes): Option<Gd<CameraAttributesPhysical>> = self
+            .base
+            .get_attributes()
+            .and_then(|attr| attr.try_cast().ok())
+        else {
+            logger::warn!("Camera has no attributes!");
             return;
         };
 
-        let sky_brightness = self.solar_setup.sky_brightness();
-        let sun_zenite_distance = self.solar_setup.sun_zenite_distance();
+        let brightness = self.solar_setup.environment_brightness();
 
-        let day_exposure = Self::MID_DAY_EXPOSURE.lerp(Self::DAWN_EXPOSURE, sun_zenite_distance);
+        let (next_index, next) = Self::FSTOPS
+            .iter()
+            .find_position(|setting| setting.lux > brightness)
+            .expect("there must always be a next");
+        let closest = &Self::FSTOPS[next_index - 1];
 
-        let night_exposure = interpolate(
-            Self::DAWN_EXPOSURE,
-            Self::NIGHT_EXPOSURE,
-            1.0 - sky_brightness,
-        );
+        let diff = next.lux - closest.lux;
 
-        let exposure = if sun_zenite_distance < 1.0 {
-            day_exposure
-        } else {
-            night_exposure
-        }
-        .floor();
+        let distance = (brightness - closest.lux).max(0.0) / diff;
 
-        if attributes.get_exposure_sensitivity() == exposure {
+        let fstop = closest.fstop.lerp(next.fstop, distance).snapped(0.1);
+        let shutter = closest.shutter.lerp(next.shutter, distance).snapped(0.1);
+
+        if attributes.get_aperture() == fstop && attributes.get_shutter_speed() == shutter {
             return;
         }
 
-        attributes.set_exposure_sensitivity(exposure);
-
-        logger::debug!(
-            "Updating camera exposure: {}",
-            attributes.get_exposure_sensitivity()
-        );
-        logger::debug!("sky brightness: {}", sky_brightness);
-    }
-}
-
-fn interpolate(from: f32, to: f32, weight: f32) -> f32 {
-    let range = to - from;
-
-    from + (weight.powf(4.0) * range)
-}
-
-#[cfg(test)]
-mod test {
-    use godot::builtin::math::ApproxEq;
-
-    use super::interpolate;
-
-    #[test]
-    fn qerp_start() {
-        let value = interpolate(4.5, 7.3, 0.0);
-
-        assert_eq!(value, 4.5);
-    }
-
-    #[test]
-    fn qerp_end() {
-        let value = interpolate(4.5, 7.3, 1.0);
-
-        assert_eq!(value, 7.3);
-    }
-
-    #[test]
-    fn qerp_sample() {
-        let value = interpolate(0.0, 1.0, 0.7);
-
-        assert!(value.approx_eq(&0.49));
+        attributes.set_aperture(fstop);
+        attributes.set_shutter_speed(shutter);
     }
 }
